d99111121cb157998840ebede7208279d2224782
Christian Guevara <chrisdb150@gmail.com>
2022-07-30 23:15:29 -0400

    SSG

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,49 @@
 import { Button } from '@nextui-org/react'
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
+import { pokeApi } from '../api';
 import { Layout } from '../components/layouts'
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
 
-const HomePage: NextPage = () => {
+interface Props {
+  pokemons: SmallPokemon[]
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+
   return (
     <Layout title='Listado de Pokémons'>
-      <Button color="gradient">
-        Hola mundo
-      </Button>
+      <ul>
+        {
+          pokemons?.map(({ id, name }) => (
+            <li key={ id }>
+              <ul>{ `${id} - ${name}` }</ul>
+            </li>
+          ))
+        }
+      </ul>
     </Layout>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, index) => (
+    {
+      name: pokemon.name,
+      url: pokemon.url,
+      id: `${index + 1}`,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`
+    }
+  ))
+
+  return {
+    props: {
+      pokemons
+    }
+  }
 }
 
 export default HomePage
