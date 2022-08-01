2f4a79d3245ed715bcfcff175fe0f353d4e8f0fe
Christian Guevara <chrisdb150@gmail.com>
2022-08-01 17:09:36 -0400

    confetti

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react';
 import { GetStaticProps, NextPage, GetStaticPaths } from 'next';
+
+import confetti from 'canvas-confetti'
+
 import { pokeApi } from '../../api';
 import { Layout } from '../../components/layouts'
 import { Pokemon } from '../../interfaces';
@@ -18,6 +21,15 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
     const onToggleFavorite = () => {
         localFavorites.toggleFavorite(pokemon.id);
         setIsInFavorites(!isInFavorites);
+
+        if (isInFavorites) return
+        confetti({
+            zIndex: 999,
+            particleCount: 100,
+            spread: 100,
+            angle: -100,
+            origin: { x: 1, y: 0 },
+        })
     }
 
 
