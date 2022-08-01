98f165a4cf5425e13a65a0c33603a5bfd4431fdc
Christian Guevara <chrisdb150@gmail.com>
2022-08-01 18:43:21 -0400

    meta tags og

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head"
 import { FC, PropsWithChildren } from "react"
+import Head from "next/head"
 
 import { Navbar } from "../ui"
 
@@ -7,7 +7,10 @@ type Props = PropsWithChildren & {
     title?: string
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 export const Layout: FC<Props> = ({ children, title = 'Pokemon App' }) => {
+
     return (
         <>
             <Head>
@@ -15,6 +18,10 @@ export const Layout: FC<Props> = ({ children, title = 'Pokemon App' }) => {
                 <meta name="autor" content="Christian Guevara" />
                 <meta name="description" content={ `Información sobre el pokémon ${title}` } />
                 <meta name="keywords" content={ `${title} pokemon, pokedex` } />
+
+                <meta property="og:title" content={ `Información sobre ${title}` } />
+                <meta property="og:description" content={ `Esta es la página sobre ${title}` } />
+                <meta property="og:image" content={ `${origin}/img/banner.png` } />
             </Head>
 
             <Navbar />
